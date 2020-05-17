708ea49c94899689741c7b4336af2e3846adb17c
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-17 17:36:01 -0400

    section-2 complete

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import { Course } from "../model/course";
+
+const config = {
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId",
+};
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+// const settings = { timestampsInSnapshots: true };
+// db.settings(settings);
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    // example of querying doc from firestore
+    // db.doc("/courses/MxCj4brrrXQPml6H1M1r")
+    //   .get()
+    //   .then((snapshot) => console.log(snapshot.data()));
 
+    // example of querying full collection
+    db.collection("courses")
+      .get()
+      .then((snapshots) => {
+        let courses: Course[] = [];
+        courses = snapshots.docs.map((snapshot) => {
+          return <Course>{ id: snapshot.id, ...snapshot.data() };
+        });
+        console.log(courses);
+      });
+  }
 }
