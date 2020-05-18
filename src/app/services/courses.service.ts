717d7ebc1c6ce645a4836f99bb66c5e468564db1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-18 09:13:08 -0400

    15 complete

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { map, first } from "rxjs/operators";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class CoursesService {
-  constructor() {}
+  constructor(private db: AngularFirestore) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.db
+      .collection("courses")
+      .snapshotChanges()
+      .pipe(
+        map((snaps) => {
+          return snaps.map((snap) => {
+            const courseData: Object = snap.payload.doc.data();
+            return <Course>{
+              id: snap.payload.doc.id,
+              ...courseData,
+            };
+          });
+        }),
+        // first is going to make sure that the value is emitted only for the first time, if there is any change in the database after the courses are loaded this operator will prevent in emitting the new data
+        first()
+      );
+  }
 }
