c7e794ddfa6da65960c9283b01a5d8b59982c7de
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-18 08:21:48 -0400

    completed untill 13

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 import { Course } from "../model/course";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 const config = {
   apiKey: "apiKey",
@@ -14,8 +15,8 @@ const config = {
   measurementId: "measurementId",
 };
 
-firebase.initializeApp(config);
-const db = firebase.firestore();
+// firebase.initializeApp(config);
+// const db = firebase.firestore();
 // const settings = { timestampsInSnapshots: true };
 // db.settings(settings);
 
@@ -25,21 +26,56 @@ const db = firebase.firestore();
   styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-  constructor() {}
+  constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
-    // example of querying doc from firestore
+    // // example of querying doc from firestore
     // db.doc("/courses/MxCj4brrrXQPml6H1M1r")
     //   .get()
     //   .then((snapshot) => console.log(snapshot.data()));
-
-    // example of querying full collection
-    db.collection("courses")
-      .get()
-      .then((snapshots) => {
-        let courses: Course[] = [];
-        courses = snapshots.docs.map((snapshot) => {
-          return <Course>{ id: snapshot.id, ...snapshot.data() };
+    // ******************************************************
+    // // example of querying full collection
+    // db.collection("courses")
+    //   .get()
+    //   .then((snapshots) => {
+    //     let courses: Course[] = [];
+    //     courses = snapshots.docs.map((snapshot) => {
+    //       return <Course>{ id: snapshot.id, ...snapshot.data() };
+    //     });
+    //     console.log(courses);
+    //   });
+    // *********************** USING ANGULAR FIRE*********************************
+    // *********************** USING Value Changes*********************************
+    this.db
+      .collection("courses")
+      .valueChanges()
+      .subscribe((val) => console.log(val));
+    // ******************** USING SnapShot changes*********************************
+    this.db
+      .collection("courses")
+      .snapshotChanges()
+      .subscribe((snaps) => {
+        const courses: Course[] = snaps.map((snap) => {
+          const courseData: Object = snap.payload.doc.data();
+          return <Course>{
+            id: snap.payload.doc.id,
+            ...courseData,
+          };
+        });
+        console.log(courses);
+      });
+    // ********************* USING State Changes*********************************
+    // state changes returns the total collection at first, but after that only changed documents are retrieved again unlike snapshot and value changes
+    this.db
+      .collection("courses")
+      .stateChanges()
+      .subscribe((snaps) => {
+        const courses: Course[] = snaps.map((snap) => {
+          const courseData: Object = snap.payload.doc.data();
+          return <Course>{
+            id: snap.payload.doc.id,
+            ...courseData,
+          };
         });
         console.log(courses);
       });
